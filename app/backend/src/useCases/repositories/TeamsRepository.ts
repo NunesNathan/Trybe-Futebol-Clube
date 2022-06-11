996e7a19f8ee0fcd63f6bb273947a62211026b0b
NunesNathan <nunesxd7@hotmail.com>
2022-06-11 17:28:10 -0300

    (model) - Get one team by pk

--- app/backend/src/useCases/repositories/TeamsRepository.ts
+++ app/backend/src/useCases/repositories/TeamsRepository.ts
@@ -1,6 +1,6 @@
 import ITeamsRepository from '../../interfaces/teamsRepository.interface';
 import TeamModel from '../../database/models/Team';
-import { Teams } from '../../interfaces/teams.interface';
+import { Team, Teams } from '../../interfaces/teams.interface';
 
 export default class TeamsRepository implements ITeamsRepository {
   private model: typeof TeamModel;
@@ -13,5 +13,11 @@ export default class TeamsRepository implements ITeamsRepository {
     const teams = await this.model.findAll();
 
     return teams;
+  }
+
+  async getByPk(id: number): Promise<Team | null> {
+    const team = await this.model.findByPk(id);
+
+    return team;
   }
 }
