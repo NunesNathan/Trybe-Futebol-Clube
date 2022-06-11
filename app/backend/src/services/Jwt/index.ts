580e3aa24ead55cd4980416553bf0b1995452f0e
NunesNathan <nunesxd7@hotmail.com>
2022-06-10 22:17:30 -0300

    (refactor) - Trying to fiz verify

--- app/backend/src/services/Jwt/index.ts
+++ app/backend/src/services/Jwt/index.ts
@@ -1,11 +1,9 @@
 import * as jwt from 'jsonwebtoken';
 import jwtConfig from '../../config/jwtConfig';
-import { UserData } from '../../interfaces/user.interface';
+import { User, UserData } from '../../interfaces/user.interface';
 
 export default class JwtLogin {
-  public verify = (token: string): UserData | null => (
-    jwt.verify(token, jwtConfig.secret)
-  ) as UserData;
+  public verify = (token: string): User | string => jwt.verify(token, jwtConfig.secret) as User;
 
   public sign = (data: UserData): string => jwt.sign({ data }, jwtConfig.secret, jwtConfig.options);
 }
