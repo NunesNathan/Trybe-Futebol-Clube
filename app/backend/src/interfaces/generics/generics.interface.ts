e04333256a4356b8f9ca95c9470880bf79b94ed7
NunesNathan <nunesxd7@hotmail.com>
2022-06-11 17:26:54 -0300

    (type) - One team adjustes

--- app/backend/src/interfaces/generics/generics.interface.ts
+++ app/backend/src/interfaces/generics/generics.interface.ts
@@ -1,9 +1,9 @@
-import { Teams } from '../teams.interface';
+import { Team, Teams } from '../teams.interface';
 import { UserDTO, UserToLogin } from '../user.interface';
 
-export type DTO = UserDTO | string;
+export type DTO = UserDTO | string | number;
 
 export type caseResponse = {
   status: number;
-  data: UserToLogin | string | Teams;
+  data: UserToLogin | string | Teams | Team;
 };
--- app/backend/src/interfaces/teamsRepository.interface.ts
+++ app/backend/src/interfaces/teamsRepository.interface.ts
@@ -1,5 +1,6 @@
-import { Teams } from './teams.interface';
+import { Team, Teams } from './teams.interface';
 
 export default interface ITeamsRepository {
-  getAll(): Promise<Teams | null>
+  getAll(): Promise<Teams | null>;
+  getByPk(id: number): Promise<Team | null>;
 }
