35be0d45acf2bf0d1da12753af5ddac9825b8ee3
NunesNathan <nunesxd7@hotmail.com>
2022-06-14 10:59:11 -0300

    (use case) - Teams id treatment

--- app/backend/src/useCases/matches/saveMatch/index.ts
+++ app/backend/src/useCases/matches/saveMatch/index.ts
@@ -1,9 +1,11 @@
 import MatchesRepository from '../../repositories/MatchesRepository';
+import TeamsRepository from '../../repositories/TeamsRepository';
 import SaveMatchController from './saveMatchController';
 import SaveMatchUseCase from './saveMatchUseCase';
 
+const teamsRepository = new TeamsRepository();
 const matchesRepository = new MatchesRepository();
-const saveMatchUseCase = new SaveMatchUseCase(matchesRepository);
+const saveMatchUseCase = new SaveMatchUseCase(matchesRepository, teamsRepository);
 const saveMatchController = new SaveMatchController(saveMatchUseCase);
 
 export default saveMatchController;
--- app/backend/src/useCases/matches/saveMatch/saveMatchUseCase.ts
+++ app/backend/src/useCases/matches/saveMatch/saveMatchUseCase.ts
@@ -1,15 +1,36 @@
 import MatchesRepository from '../../repositories/MatchesRepository';
+import TeamsRepository from '../../repositories/TeamsRepository';
 import UseCase from '../../../interfaces/generics/useCases.abstract';
 import IErrorHttp from '../../../middleware/error/errorHttp';
 import { caseResponse } from '../../../interfaces/generics/generics.interface';
 import { ReqSaveMatch } from '../../../interfaces/matches.interface';
 
 export default class SaveMatchUseCase extends UseCase {
-  constructor(private matchesRepository: MatchesRepository) {
+  constructor(
+    private matchesRepository: MatchesRepository,
+    private teamsRepository: TeamsRepository,
+  ) {
     super();
   }
 
+  private async existTeam(team: number): Promise<boolean> {
+    const exists = await this.teamsRepository.getByPk(team);
+
+    return !!exists;
+  }
+
   public async execute(body: ReqSaveMatch): Promise<caseResponse> {
+    if (body.homeTeam === body.awayTeam) {
+      throw new IErrorHttp(401, 'It is not possible to create a match with two equal teams');
+    }
+
+    const existsHome = await this.existTeam(body.homeTeam);
+    const existsAway = await this.existTeam(body.awayTeam);
+
+    if (!existsAway || !existsHome) {
+      throw new IErrorHttp(404, 'There is no team with such id!');
+    }
+
     const match = await this.matchesRepository.save(body);
 
     if (!match) throw new IErrorHttp(404, 'Not Found');
