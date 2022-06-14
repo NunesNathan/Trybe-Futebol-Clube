1d2aa44473a03af6ee840a47ff106d6e31a4e688
NunesNathan <nunesxd7@hotmail.com>
2022-06-14 09:58:02 -0300

    (router) - Save match route works

--- app/backend/src/routers/match.ts
+++ app/backend/src/routers/match.ts
@@ -3,6 +3,7 @@ import matches from '../useCases/matches/matches';
 
 const router = Router();
 
-router.get('/', matches.allMatches);
+router.get('/', matches.allMatches)
+  .post('/', matches.saveMatch);
 
 export default router;
--- app/backend/src/useCases/matches/matches.ts
+++ app/backend/src/useCases/matches/matches.ts
@@ -1,7 +1,15 @@
+import JwtValidation from '../../middleware/validate/jwtValidation';
 import allMatchesController from './allMatches';
+import saveMatchController from './saveMatch';
+
+const jwtValidation = new JwtValidation();
 
 const matches = {
   allMatches: allMatchesController.handle.bind(allMatchesController),
+  saveMatch: [
+    jwtValidation.handle.bind(jwtValidation),
+    saveMatchController.handle.bind(saveMatchController),
+  ],
 };
 
 export default matches;
