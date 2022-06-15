761ac6b841072184666308f8d28c0b5bbb88a62f
NunesNathan <nunesxd7@hotmail.com>
2022-06-15 19:31:35 -0300

    (model) - Edit matches

--- app/backend/src/useCases/repositories/MatchesRepository.ts
+++ app/backend/src/useCases/repositories/MatchesRepository.ts
@@ -1,7 +1,7 @@
 import IMatchesRepository from '../../interfaces/matchesRepository.interface';
 import TeamModel from '../../database/models/Team';
 import MatchModel from '../../database/models/Match';
-import { Matches, ModelMatch, ReqSaveMatch } from '../../interfaces/matches.interface';
+import { Matches, ModelMatch, ReqSaveMatch, UpdateBody } from '../../interfaces/matches.interface';
 
 export default class MatchesRepository implements IMatchesRepository {
   private model: typeof MatchModel;
@@ -30,6 +30,16 @@ export default class MatchesRepository implements IMatchesRepository {
   async finish(id: number): Promise<boolean> {
     const [match] = await this.model
       .update({ inProgress: false }, { where: { id } }) as unknown as number[];
+
+    return !!match;
+  }
+
+  async update({ id, body }: UpdateBody): Promise<boolean> {
+    const [match] = await this.model
+      .update({
+        homeTeamGoals: body.homeTeamGoals,
+        awayTeamGoals: body.awayTeamGoals,
+      }, { where: { id } });
 
     return !!match;
   }
